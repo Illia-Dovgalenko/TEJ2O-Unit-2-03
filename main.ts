172abfed59a9b054f5c32e5710d2c7c4deb57de2
Illia-Dovgalenko <illia.dovgalenko@stu.ocsb.ca>
2026-03-09 19:23:18 +0200

    basic math

--- main.ts
+++ main.ts
@@ -1,8 +1,16 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Illia
+ * Created on: March 2026
+ * This program basic math
 */
 
-basic.showString('Hello, World!')
+basic.showString('A rectangle has dimensions 5 cm and 3 cm.')
+
+basic.showString('The perimeter would be:' + (2 * 5 + 3).toString())
+
+basic.showString('cm')
+
+basic.showString('The area would be:' + (5 * 3).toString())
+
+basic.showString('cm^2')
